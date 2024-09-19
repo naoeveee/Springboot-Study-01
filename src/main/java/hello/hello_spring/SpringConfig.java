afd9ddd14a87a047b9445939e5b13480e1bb6342
jamie <jamiechoi999@gmail.com>
2024-09-19 17:18:45 +0900

    0919 3주차 스터디

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    //멤버 리포지토리를 멤버 서비스에 넣어줌
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
