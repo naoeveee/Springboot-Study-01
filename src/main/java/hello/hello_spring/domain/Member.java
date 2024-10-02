e23b9ff5a0706a602dfb480798bcf5d871cc953d
jamie <jamiechoi999@gmail.com>
2024-10-02 16:09:30 +0900

    0926 4주차 스터디

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -5,19 +5,19 @@ public class Member {
     private Long id;
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
